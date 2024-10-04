7eba535d0707ef5e6519fb36ddee775757ac2ad7
Enes Oguz <eoguz@a2b-online.com>
2024-10-04 13:00:02 +0200

    all files staged and ready for push

--- Main.java
+++ Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.setMake("porsche");
+        car.setModel("Carrera");
+        car.setColor("Red");
+        car.setDoors(4);
+        car.setConvertible(true);
+        System.out.println("Make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+    }
 }
